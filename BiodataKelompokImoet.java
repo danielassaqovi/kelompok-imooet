08c5e99c4d3fa1b946c06a1db5bd3e2b04908028
m-fatahillah <fatahcrown789@gmail.com>
2024-09-05 15:44:07 +0700

    FATAH

--- BiodataKelompokImoet.java
+++ BiodataKelompokImoet.java
@@ -1,5 +1,7 @@
 public class BiodataKelompokImoet {
   public static void main(String[] args) {
-      System.out.println("");
+      System.out.println("Nama : Muhammad Fatahillah Athabrani");
+      System.out.println("NIM  : 244107020121");
   }
 }
+
